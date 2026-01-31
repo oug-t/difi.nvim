0fbbdcddc391bc091d6441ec6c4c34c8d4cf2562
Tommy Guo <tommyguo024@outlook.com>
2026-01-31 14:04:01 -0500

    feat: implement support for difi

--- plugin/difi.lua
+++ plugin/difi.lua
@@ -10,3 +10,16 @@ end, { nargs = '?' })
 vim.api.nvim_create_user_command("DifiHealth", function()
     require("difi.health").check()
 end, {})
+
+local env_target = vim.env.DIFI_TARGET
+
+if env_target then
+    vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+            if vim.fn.filereadable(vim.fn.expand("%")) == 1 then
+                require("difi").toggle(env_target)
+            end
+        end,
+        once = true
+    })
+end
